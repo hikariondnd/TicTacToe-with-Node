dd620f7144edf0278b072ca7815dd9fe1c3e9eac
Gwyneth Rivera <130489542+hikariondnd@users.noreply.github.com>
2024-03-04 11:11:34 -0500

    Update TicTacToe.js
    Done on Visual Studio Code JavaScript

--- TicTacToe.js
+++ TicTacToe.js
@@ -18,7 +18,7 @@ function Board({ xIsNext, squares, onPlay}) {
         if (xIsNext) {
             nextSquares[i] = 'X';
         } else {
-            next squares[i] = 'O';
+            nextSquares[i] = 'O';
         }
         onPlay(nextSquares);
     }
@@ -35,5 +35,95 @@ function Board({ xIsNext, squares, onPlay}) {
             <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareclick={() => handleClick(0)} />
-    )
+                <Square value={squares[1]} onSquareclick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareclick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareclick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareclick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareclick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareclick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareclick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareclick={() => handleClick(8)} />
+            </div>
+        </>
+    );
 }
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 ===0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+        setXIsNext(!xIsNext);
+    }
+
+    function jumpTo(NextMove) {
+        setCurrentMove(nextMove);
+        setXIsNext(nextMove % 2 === 0);
+    }
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        }  else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{move}</ol>
+            </div>
+        </div>
+    );
+}
+function calculateWinner(squares) {
+    const lines  = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+export default function Square() {
+    return <button className="square">X</button>;
+}
+
+import { StrictMode } from 'react';
+import { cresteRoot } from 'react-dom/cliemt';
+import './stykes.css';
+import App from './App';
+
+export default function Square() {
+    return <button className="square">X</button><button className="square">X</button>;
+}
+
